55085e69bebf7538083947f1d2a576232d3c83b2
tonyskinner <tony@sliide.com>
2021-09-02 17:38:13 +0100

    Images works, now to prompt for the path, do video and audio

--- folder_operations.py
+++ folder_operations.py
@@ -14,3 +14,10 @@ class FolderOperations:
 
     def does_folder_exist_in_image_path(self, folder_name):
         return path.exists(f"{self.image_folder_path}/{folder_name}")
+
+    def move_file_to_folder(self, file_name, new_folder_path):
+        current_image_path = f"{self.image_folder_path}/{file_name}"
+        new_image_path = f"{self.image_folder_path}/{new_folder_path}/{file_name}"
+
+        os.rename(current_image_path, new_image_path)
+        logging.info(f"Moved {file_name} to {new_folder_path}")
--- main.py
+++ main.py
@@ -18,6 +18,14 @@ logging.basicConfig(
 )
 
 
+def create_year_and_month_structure_if_it_does_not_exist(image_details):
+    create_folder_if_it_does_not_exist(folder_name=image_details['modified_year'])
+
+    create_folder_if_it_does_not_exist(
+        folder_name=f"{image_details['modified_year']}/{image_details['modified_month']}"
+    )
+
+
 def create_folder_if_it_does_not_exist(folder_name):
     if not folder_operations.does_folder_exist_in_image_path(folder_name):
         folder_operations.create_folder(folder_name=folder_name)
@@ -33,13 +41,18 @@ if __name__ == '__main__':
     image_operations = image_operations.ImageOperations(image_folder_path)
     folder_operations = folder_operations.FolderOperations(image_folder_path)
 
-    # print("Last modified: %s" % time.ctime(os.path.getmtime("/Users/tonyskinner/Documents/phoneImages/IMG_20190201_070709.jpg")))
-    # print(time.ctime(os.path.getmtime("/Users/tonyskinner/Documents/phoneImages/IMG_20190201_070709.jpg")))
-    # print("Created: %s" % time.ctime(os.path.getctime("/Users/tonyskinner/Documents/phoneImages/IMG_20190201_070709.jpg")))
-
     image_list = get_list_of_images_in_path(image_folder_path)
 
     for image in image_list:
         image_details = image_operations.get_image_last_modified_details(image)
 
-        create_folder_if_it_does_not_exist(folder_name=image_details['modified_year'])
+        create_year_and_month_structure_if_it_does_not_exist(image_details)
+
+        image_new_folder_path = f"{image_details['modified_year']}/{image_details['modified_month']}" \
+                                f"/{image_details['modified_date']}"
+
+        create_folder_if_it_does_not_exist(folder_name=image_new_folder_path)
+
+        folder_operations.move_file_to_folder(file_name=os.path.basename(image), new_folder_path=image_new_folder_path)
+
+
