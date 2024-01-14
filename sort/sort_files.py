c46970f60d4bbd3f3685d88af5eab1caafa6af99
tonyskinner <tony@sliide.com>
2024-01-14 10:14:57 +0000

    alomst refactored the moving

--- sort/sort_files.py
+++ sort/sort_files.py
@@ -1,8 +1,10 @@
+import logging
 import os
 
 from system_operations import file_operations
 from system_operations import folder_operations
 from config import setup
+from pathlib import Path
 
 
 class SortFiles:
@@ -12,39 +14,36 @@ class SortFiles:
         self.file_operations = file_operations.FileOperations(filepath)
         self.folder_operations = folder_operations.FolderOperations(filepath)
 
-    def create_year_and_month_structure_if_it_does_not_exist(self, file_details, file_type):
-        year_folder_path = f"{file_type}/{file_details['modified_year']}"
-        month_folder_path = f"{year_folder_path}/{file_details['modified_month']}"
-
-        self.create_folder_if_non_existent(folder_name=year_folder_path)
-        self.create_folder_if_non_existent(folder_name=month_folder_path)
-
-    def create_folder_if_non_existent(self, folder_name):
-        if not self.folder_operations.does_folder_exist_in_path(folder_name):
-            self.folder_operations.create_folder(folder_name=folder_name)
-
     def create_initial_folder_structure(self):
         for folder_name in self.system_config['initial_folder_structure']:
-            self.create_folder_if_non_existent(folder_name)
+            new_folder_filepath = f"{self.filepath}/{folder_name}"
+            self.folder_operations.create_filepath(new_folder_filepath)
+            logging.info(f"Created the folder {new_folder_filepath} if it does not already exist")
 
     def process_files_in_file_list(self, list_of_files, file_type):
         for file in list_of_files:
+            file_name = os.path.basename(file)
             file_details = self.file_operations.get_file_last_modified_details(file)
 
-            # self.create_year_and_month_structure_if_it_does_not_exist(file_details=file_details, file_type=file_type)
+            new_file_folder_path = f"{self.filepath}/" \
+                                   f"{file_type}/" \
+                                   f"{file_details['modified_year']}/" \
+                                   f"{file_details['modified_month']}" \
+                                   f"/{file_details['modified_date']}"
 
-            sorted_file_new_folder_path = f"{file_type}/" \
-                                          f"{file_details['modified_year']}/" \
-                                          f"{file_details['modified_month']}" \
-                                          f"/{file_details['modified_date']}"
+            self.folder_operations.create_filepath(new_file_folder_path)
+            logging.info(f"Creating the path {new_file_folder_path} if it does not exist")
 
-            print(sorted_file_new_folder_path)
+            current_file_folder_path = f"{self.filepath}/{file_name}"
 
-            # self.create_initial_folder_structure(folder_name=sorted_file_new_folder_path)
-            #
-            # self.folder_operations.move_file_to_folder(file_name=os.path.basename(file),
-            #                                            new_folder_path=sorted_file_new_folder_path)
+            print(current_file_folder_path)
+            print(new_file_folder_path)
+            quit()
 
+            self.folder_operations.move_file_to_folder(current_path=current_file_folder_path,
+                                                       new_path=new_file_folder_path)
+
+            logging.info(f"Moved {file_name} to {new_file_folder_path}")
 
     def sort_files_into_folders(self):
         self.create_initial_folder_structure()
@@ -58,4 +57,6 @@ class SortFiles:
         audio_files_list = self.file_operations.get_list_of_files_in_path_by_type(
             file_extension_types=self.system_config['audio_file_extensions'])
 
-        self.process_files_in_file_list(image_files_list)
+        self.process_files_in_file_list(image_files_list, "Images")
+        self.process_files_in_file_list(video_files_list, "Videos")
+        self.process_files_in_file_list(audio_files_list, "Audio")
--- system_operations/folder_operations.py
+++ system_operations/folder_operations.py
@@ -1,23 +1,14 @@
 import logging
 import os
-from os import path
+from pathlib import Path
 
 
 class FolderOperations:
     def __init__(self, folder_path):
         self.folder_path = folder_path
 
-    def create_folder(self, folder_name):
-        new_folder_path = f"{self.folder_path}/{folder_name}"
-        os.mkdir(new_folder_path)
-        logging.info(f"Created the folder {new_folder_path}")
+    def create_filepath(self, filepath):
+        Path(filepath).mkdir(parents=True, exist_ok=True)
 
-    def does_folder_exist_in_path(self, folder_name):
-        return path.exists(f"{self.folder_path}/{folder_name}")
-
-    def move_file_to_folder(self, file_name, new_folder_path):
-        current_path = f"{self.folder_path}/{file_name}"
-        new_path = f"{self.folder_path}/{new_folder_path}/{file_name}"
-
+    def move_file_to_folder(self, current_path, new_path):
         os.rename(current_path, new_path)
-        logging.info(f"Moved {file_name} to {new_folder_path}")
